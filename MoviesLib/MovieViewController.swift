2e10f393810daee1cd8116d6c7b018f78a844be7
ThomasGalvao <thom.galvao@gmail.com>
2018-03-24 16:56:29 -0300

    Initial Commit

---
+++ MoviesLib/MovieViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Eric on 06/03/17.
+//  Copyright © 2017 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    // MARK: IBOutlets
+    @IBOutlet weak var ivPoster: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbGenre: UILabel!
+    @IBOutlet weak var lbDuration: UILabel!
+    @IBOutlet weak var lbScore: UILabel!
+    @IBOutlet weak var tvSinopsis: UITextView!
+    
+    // MARK: Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        lbTitle.text = "Logan, the movie \nHoje nos Cinemas!"
+    }
+
+}
+
