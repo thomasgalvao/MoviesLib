839dcca444fe0ac4502ad7ce529ab3db09b5250f
ThomasGalvao <thom.galvao@gmail.com>
2018-03-28 20:26:34 -0300

    ok novo ajuste

---
+++ MoviesLib/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 28/03/18.
+//  Copyright © 2018 EricBrito. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let title: String
+    let categories: String
+    let duation: String
+    let rating: Double
+    let summary: String
+    let image_name: String
+}
